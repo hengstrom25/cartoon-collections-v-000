ff21ad9ee2f7214d10c7ee36826e72d2c2be5e67
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-24 23:26:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,7 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls()
 
 def find_the_cheese# code an argument here
   # the array below is here to help
