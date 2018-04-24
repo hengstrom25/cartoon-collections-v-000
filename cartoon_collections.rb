ecfafd49c13e9c2bdffc7816d481cad79c8d3d47
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-24 23:22:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planteer_calls.map 
+  planteer_calls.map do |words|
+    
 end
 
 def long_planeteer_calls# code an argument here
