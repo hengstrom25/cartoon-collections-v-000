de447f403f12b8d0108d54aa3ed79d63d5962e29
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-25 00:18:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  if words.any? |words|
-    words.length > 4
-    true
+  if words.any? {|words|
+    words.length > 4}
   end
 end
 
